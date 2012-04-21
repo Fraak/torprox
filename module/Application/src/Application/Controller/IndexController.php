9fe682d4df2e375b75a2880252f0d99bb90ea216
unknown <Freek@Freek-PC.(none)>
2012-04-21 22:26:47 +0200

    Initial commit

---
+++ module/Application/src/Application/Controller/IndexController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\ActionController,
+    Zend\View\Model\ViewModel;
+
+class IndexController extends ActionController
+{
+    /**
+     * @param string $query
+     * @return \Zend\Feed\Reader\Reader
+     */
+    private function getRss($query)
+    {
+        $reader = \Zend\Feed\Reader\Reader::import('http://torrentz.eu/feed?q='.$query);
+
+        /** @var \Zend\Feed\Reader\Entry\Rss $entry */
+        foreach($reader as $entry)
+        {
+            /** @var \DOMDocument $dom */
+            preg_match('/Size: ([0-9]* MB) Seeds: ([0-9]*) Peers: ([0-9]*) Hash: ([0-9a-z]*)/', $entry->getDescription(), $matches);
+            $dom = $entry->getDomDocument();
+            $element = $entry->getElement();
+            $element->appendChild($dom->createElement('size', $matches[1]));
+            $element->appendChild($dom->createElement('seeds', $matches[2]));
+            $element->appendChild($dom->createElement('peers', $matches[3]));
+
+            $closure = $dom->createElement('enclosure');
+            $closure->setAttribute('url', 'magnet:?xt=urn:btih:'.strtoupper($matches[4]).'&dn='.$entry->getTitle());
+            $closure->setAttribute('type', 'application/x-bittorrent');
+            $element->appendChild($closure);
+        }
+
+        return $reader;
+    }
+
+    /**
+     * @return string
+     */
+    private function getQuery()
+    {
+        return $this->getRequest()->query()->get('query');
+    }
+
+    /**
+     * @return ViewModel
+     */
+    private function getViewModel()
+    {
+        $query = $this->getQuery();
+        if($query != null)
+        {
+            return new ViewModel(array(
+                'feed' => $this->getRss($query),
+                'query' => $query
+            ));
+        }
+
+        return new ViewModel(array('query' => $query));
+    }
+
+    public function indexAction()
+    {
+        return $this->getViewModel();
+    }
+
+    public function rssAction()
+    {
+        $this->getResponse()->headers()->addHeaderLine('content-type', 'application/xml');
+        $model = $this->getViewModel();
+        $model->setTerminal(true);
+        return $model;
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php
+
+date_default_timezone_set('europe/amsterdam');
+
+chdir(dirname(__DIR__));
+require_once 'vendor/ZendFramework/library';
+Zend\Loader\AutoloaderFactory::factory();
+
+$appConfig = include 'config/application.config.php';
+
+$listenerOptions  = new Zend\Module\Listener\ListenerOptions($appConfig['module_listener_options']);
+$defaultListeners = new Zend\Module\Listener\DefaultListenerAggregate($listenerOptions);
+$defaultListeners->getConfigListener()->addConfigGlobPath("config/autoload/{,*.}{global,local}.config.php");
+    
+
+$moduleManager = new Zend\Module\Manager($appConfig['modules']);
+$moduleManager->events()->attachAggregate($defaultListeners);
+$moduleManager->loadModules();
+
+// Create application, bootstrap, and run
+$bootstrap   = new Zend\Mvc\Bootstrap($defaultListeners->getConfigListener()->getMergedConfig());
+$application = new Zend\Mvc\Application;
+$bootstrap->bootstrap($application);
+$application->run()->send();
